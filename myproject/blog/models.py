d9a8d350697bd7b44889f9c23dd401d48c2a94e8
Bhumi Joshi <bhumij.tagline@gmail.com>
2024-03-14 17:39:29 +0530

    home-page

---
+++ myproject/blog/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+from django.utils import timezone
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        db_table = "base_model"
+
+class Author(BaseModel):
+    name = models.CharField(max_length=50)
+    biological_info = models.TextField()
+    
+    class Meta:
+        db_table = "author"
+    
+    
+    
+class BlogPost(BaseModel):
+    blog_title = models.CharField(max_length=200)
+    post_date = models.DateTimeField("date posted")
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    content = models.TextField()
+    
+    class Meta:
+        db_table = "blog_post"
+    
+class Comment(BaseModel):
+    blog = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
+    comment_post_date = models.DateTimeField(auto_now_add=True)
+    comment = models.TextField()
+    
+    
+    
+
+# from django.db import models
+
+
+# class TimeStampedModel(models.Model):
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+
+#     class Meta:
+#         abstract = True
+
+
+# class Member(TimeStampedModel):
+#     member_id = models.CharField(max_length=255)
+
+
+# class EliteMember(Member, TimeStampedModel):
+#     additional_elite_data = models.TextField()
