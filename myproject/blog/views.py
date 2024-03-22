342c38d490353de361611d7e4097066734038f29
Bhumi Joshi <bhumij.tagline@gmail.com>
2024-03-22 17:09:01 +0530

    comment form

--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -1,10 +1,11 @@
 from typing import Any
 from django.db.models.query import QuerySet
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import redirect, render
 from django.views import View, generic
-from .models import BlogPost, Author
+from .models import BlogPost, Author, Comment
 from django.conf import settings
-
+from .form import CommentForm
+from django.views.generic.edit import FormMixin
  
 class HomeView(View):
     def get(self, request):
@@ -28,17 +29,28 @@ class AuthorProfile(generic.DetailView):
         context['author_posts'] = author.blogs.all().order_by("-created_at")
         return context
 
-class BlogDetail(generic.DetailView):
+class BlogDetail(generic.DetailView,FormMixin):
     model = BlogPost
     template_name = "blog/blog_detail.html"
     context_object_name = "post"
+    form_class = CommentForm 
     
     def get_context_data(self, **kwargs: Any):
         context = super().get_context_data(**kwargs)
         post = self.get_object()
         context['comments'] = post.comments.all().order_by("created_at")
+        context['form'] = CommentForm()
         return context
     
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST) 
+        post = self.get_object() 
+         
+        if form.is_valid():
+            comment = form.cleaned_data['comment']
+            comment_instance = Comment.objects.create(blog=post, comment=comment)
+            return redirect('.')
+            
 class BloggerList(generic.ListView):
      model = Author
      paginate_by = settings.DEFAULT_PAGINATED_RECORDS
